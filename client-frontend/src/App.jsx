67360925c334fcbdba5e45bfef944f744575fd59
noob-bie <mohsinnihal55@gmail.com>
2025-02-15 22:55:27 +0600

    Category-Link

--- client-frontend/src/App.jsx
+++ client-frontend/src/App.jsx
@@ -10,10 +10,10 @@ import AdminPanel from "./pages/adminPanel/AdminPanel";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import { element } from "prop-types";
-import Healthcare from "./pages/healthcare/Healthcare";
-import Homecare from "./pages/homecare/Homecare";
-import Medicines from "./pages/medicine/Medicines";
-import BabyAndMomcare from "./pages/babyandmomcare/BabyAndMomcare";
+import Healthcare from "./pages/shop/healthcare/Healthcare";
+import Homecare from "./pages/shop/homecare/Homecare";
+import Medicines from "./pages/shop/medicine/Medicines";
+import BabyAndMomcare from "./pages/shop/babyandmomcare/BabyAndMomcare";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -35,15 +35,21 @@ function App() {
       element: <Layout />,
       children: [
         { path: "/", element: <Home /> },
-        { path: "/shop", element: <Shop /> },
-        { path: "/healthcare", element: <Healthcare /> },
-        {path :"/homecare",element:<Homecare/>},
-        {path:"/medicines",element:<Medicines/>},
-        {path:"/baby&momcare",element:<BabyAndMomcare/>},
+        {
+          path: "/shop",
+          element: <Shop />, // Shop is now a parent route
+          children: [
+            { path: "healthcare", element: <Healthcare /> },
+            { path: "homecare", element: <Homecare /> },
+            { path: "medicines", element: <Medicines /> },
+            { path: "baby&momcare", element: <BabyAndMomcare /> },
+          ],
+        },
+
         { path: "/cart", element: <Cart /> },
         { path: "/profile", element: <Profile /> },
-        { path: "/login", element: <Login/>},
-        { path: "/signup", element: <Signup/>},
+        { path: "/login", element: <Login /> },
+        { path: "/signup", element: <Signup /> },
         isAdmin && { path: "/admin", element: <AdminPanel /> },
       ].filter(Boolean),
     },
