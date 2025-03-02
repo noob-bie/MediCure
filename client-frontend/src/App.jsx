60a9a85e6a5a3d29502f9fe822bdd978a358805b
noob-bie <mohsinnihal55@gmail.com>
2025-03-02 15:03:07 +0600

    P2

--- client-frontend/src/App.jsx
+++ client-frontend/src/App.jsx
@@ -20,7 +20,7 @@ import Payment from "./pages/payment/Payment";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isAdmin, setIsAdmin] = useState(false);
+  const isAdmin = localStorage.getItem("userRole") === "admin";
   const [cartItems, setCartItems] = useState([]); // Initialize cartItems state
 
 
@@ -65,7 +65,7 @@ function App() {
         { path: "/shop/homecare/:productName", element: <SingleProduct /> },
         { path: "/shop/medicines/:productName", element: <SingleProduct /> },
 
-        isAdmin && { path: "/admin", element: <AdminPanel /> },
+        isAdmin ? { path: "/admin", element: <AdminPanel /> }: null,
       ].filter(Boolean),
     },
   ]);
